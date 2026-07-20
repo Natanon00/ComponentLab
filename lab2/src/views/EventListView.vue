4131c1a76eb434c9fe6c858722004fd4d58ed72b
Natanon00 <natanon.somboon2549@gmail.com>
2026-07-20 11:40:18 +0700

    complete 15.3

--- lab2/src/views/EventListView.vue
+++ lab2/src/views/EventListView.vue
@@ -2,42 +2,21 @@
 import EventCard from '@/components/EventCard.vue'
 import EventMeta from '@/components/EventMeta.vue'
 import type { Event } from '@/types'
-import { ref } from 'vue'
-const events = ref <Event[]>([
-  {
-  id: 5028101,
-  category: 'animal welfare',
-  title: 'Cat Adoption Day',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meow Town',
-  date: 'January 28, 2026',
-  time: '12:00',
-  petsAllowed: true,
-  organizer: 'Kat Laydee'
-  },
-  {
-    id: 4582797,
-    category: 'food',
-    title: 'Community Gardening',
-    description: 'Join us as we tend to the community edible plants.',
-    location: 'Flora Ciry',
-    date: 'March 14, 2026',
-    time: '10:00',
-    petsAllowed: true,
-    organizer: 'Fern Pollin'
-  },
-  {
-    id: 8419988,
-    category: 'sustainability',
-    title: 'Beach Cleanup',
-    description: 'Help pick up trash along the shore.',
-    location: 'Playa Del Carmen',
-    date: 'July 22, 2026',
-    time: '11:00',
-    petsAllowed: false,
-    organizer: 'Carey Wales'
-  }
-])
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const events = ref<Event[] | null>(null)
+
+onMounted(() => {
+  axios
+    .get('https://my-json-server.typicode.com/Natanon00/db/events')
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
 </script>
 <template>
 <h1>Event For Good</h1>
